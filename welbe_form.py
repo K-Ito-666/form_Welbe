33dea1f3a0b7fdfbe4e78b51add36a9ca0593304
K-Ito-666 <85495298+K-Ito-666@users.noreply.github.com>
2022-06-07 22:45:17 +0900

    Update welbe_form.py

--- welbe_form.py
+++ welbe_form.py
@@ -11,7 +11,7 @@ import plotly.figure_factory as ff
 import plotly.express as px
 
 mood=["幸せではない","やや幸せではない","ふつう","やや幸せ","幸せ"]
-happy_score = [0,1,2,3,4,5,6,7,8,9,10]
+happy_score = [,0,1,2,3,4,5,6,7,8,9,10]
 
 st.title('個と場のWell-being日記')
 diary = st.text_area(label='A：3行程度で日記をご記入ください（仕事に無関係でも構いません）', height=12)
@@ -24,8 +24,8 @@ with st.expander("クリックで日記の入力例を表示します"):
     st.caption('入力例6：旦那は気楽に1人で外出出来ていいなー。決して娘と一緒に居るのが嫌な訳じゃないけど…たまには1人で買い物行きたいなー')
     st.caption('入力例7：最近毎日雨降ってる気がする。洗濯物干せないとかはまだいいけど、何より傘持ったまま朝から満員電車に乗るのが辛すぎる。')
 
-my_happy = st.radio("B：あなたは今日一日幸せでしたか？（0点:とても不幸／10点：とても幸せ）",options=happy_score,default=None,horizontal=True)
-group_happy = st.radio('C：チーム全体としては，今日一日幸せだったと思いますか？（0点:とても不幸／10点：とても幸せ）',options=happy_score,default=None,horizontal=True)
+my_happy = st.radio("B：あなたは今日一日幸せでしたか？（0点:とても不幸／10点：とても幸せ）",options=happy_score,horizontal=True)
+group_happy = st.radio('C：チーム全体としては，今日一日幸せだったと思いますか？（0点:とても不幸／10点：とても幸せ）',options=happy_score,horizontal=True)
 location = st.selectbox(
     'D：業務中，主に滞在した場所をお選び下さい',
     ('社内の自席', '会議スペース', 'オープンスペース', '自宅', 'その他')
