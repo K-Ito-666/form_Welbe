3eecd5d0d38283a55ac87f8f4c244d843c89e598
K-Ito-666 <85495298+K-Ito-666@users.noreply.github.com>
2022-06-20 22:31:33 +0900

    Add files via upload

--- welbe_form.py
+++ welbe_form.py
@@ -9,9 +9,11 @@ import datetime
 import copy
 import plotly.figure_factory as ff
 import plotly.express as px
+
 import json
 import streamlit_authenticator as stauth
 
+mood=["幸せではない","やや幸せではない","ふつう","やや幸せ","幸せ"]
 happy_score = ['選択して下さい（0〜10点）',0,1,2,3,4,5,6,7,8,9,10]
 today = datetime.date.today()
 day_list=[]
@@ -40,7 +42,7 @@ def main():
 
     if st.button('SUBMIT') == True:
         st.write('入力完了しました！')
-        f=open('data/dailyreport.json','r')
+        f=open('dailyreport.json','r')
         j_r = json.load(f)
         if str(day) not in j_r.keys():
             j_r[str(day)]={'text':{},'my_happy':{},'group_happy':{},'location':{},'location_other':{}}
@@ -50,10 +52,10 @@ def main():
         j_r[str(day)]['location'][name]=location
         j_r[str(day)]['location_other'][name]=location_other
         
-        with open('data/dailyreport.json','w') as j_w:
+        with open('./data/dailyreport.json','w') as j_w:
             json.dump(j_r,j_w)
 
-    with open('data/dailyreport.json','r') as j_r2:
+    with open('./data/dailyreport.json','r') as j_r2:
         dictDB = json.load(j_r2)
         for days in dictDB.keys():
             if name in dictDB[days]['text'].keys():
@@ -68,19 +70,35 @@ def main():
         st.table(data=df_diary)
 
 
-    df = pd.read_excel('data/sample_dailyreport.xlsx')
+    with open('./data/dailyreport.json','r') as j_r_saved:
+        dict_diaryDB = json.load(j_r_saved)
+
+    happy_day_list=[]
+    happy_list=[]
+    for days in dict_diaryDB.keys():
+        for my_happy_scores in dict_diaryDB[days]['my_happy'].values():
+            if type(my_happy_scores) != str:
+                happy_day_list.append(days)
+                happy_list.append(my_happy_scores)
+
+    df_saved_happy = pd.DataFrame()
+    df_saved_happy['date']=happy_day_list
+    df_saved_happy['happy_score']=happy_list
+
+    st.table(df_saved_happy)
+
     st.subheader('Team Well-being Timeline')
-    line = alt.Chart(df).mark_line(
+    line = alt.Chart(df_saved_happy).mark_line(
         color='red'
     ).encode(
         x=alt.X('date:T',axis=alt.Axis(format="%m月%d日",labelFontSize=14, ticks=False, titleFontSize=18,title='日付')),
-        y=alt.Y('mean(P):Q',axis=alt.Axis(titleFontSize=18, title='Team Well-being'))
+        y=alt.Y('mean(happy_score):Q',axis=alt.Axis(titleFontSize=18, title='Team Well-being'))
     ).properties(
         width=650,
         height=400,
         )
 
-    points = alt.Chart(df).mark_point().encode(
+    points = alt.Chart(df_saved_happy).mark_point().encode(
         x=alt.X('date:T'),
         y=alt.Y('P:Q')
         ).properties(
@@ -92,9 +110,9 @@ def main():
 
 
 # ユーザ情報。引数
-names = ['admin','003', '004'] 
-usernames = ['admin','001','002','003', '004']  # 入力フォームに入力された値と合致するか確認される
-passwords = ['admin','001','002','003', '004']  # 入力フォームに入力された値と合致するか確認される
+names = ['admin','001','002','003','004'] 
+usernames = ['admin','001','002','003','004']  # 入力フォームに入力された値と合致するか確認される
+passwords = ['admin','001','002','003','004']  # 入力フォームに入力された値と合致するか確認される
 
 # パスワードをハッシュ化。 リスト等、イテラブルなオブジェクトである必要がある
 hashed_passwords = stauth.Hasher(passwords).generate()
