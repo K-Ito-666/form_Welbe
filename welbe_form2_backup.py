e2f5efe8da515cb05f5eb436964b3657f109d9fb
K-Ito-666 <85495298+K-Ito-666@users.noreply.github.com>
2022-07-06 15:53:21 +0900

    Update welbe_form2_backup.py

--- welbe_form2_backup.py
+++ welbe_form2_backup.py
@@ -125,7 +125,6 @@ st.title('個と場のWell-being日記')
 name, authentication_status, username = authenticator.login('Login', 'main')
 
 st.write(cred)
-st.write(authentication)
 
 # 返り値、authenticaton_statusの状態で処理を場合分け
 if authentication_status:
